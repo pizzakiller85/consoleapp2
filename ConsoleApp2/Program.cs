d05143a01f4bf102c831c942937249c1c47580a3
Dan <danielchapko@gmail.com>
2021-07-05 15:46:19 -0400

    added unit test

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,6 +7,26 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine(add(1, 3));
+        }
+
+        public static int add(int one, int two)
+        {
+            return one + two;
         }
     }
+
+
+
+    public class numOpp
+    {
+
+        public int addT(int one, int two)
+        {
+            return one + two;
+        }
+
+    }
+
 }
---
+++ ConsoleApp2Tests/numOppTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Tests
+{
+    [TestClass()]
+    public class numOppTests
+    {
+        [TestMethod()]
+        public void addTTest()
+        {
+
+            var tt = new numOpp() { };
+            var test = tt.addT(1, 2);
+
+            Assert.AreEqual(3, test);
+        }
+    }
+}
+
+namespace ConsoleApp2Tests
+{
+    class numOppTests
+    {
+    }
+}
